9e34ef4b34a875e7dc964cb4c920eea5c523f6ff
nadashaban10 <nadashaban10@github.com>
2024-12-30 05:08:45 +0200

    sample

--- src/App.js
+++ src/App.js
@@ -5,10 +5,10 @@ import NavBar from "./components/NavBar";
 function App() {
     return (
       <>
-      <div className="">
+   
      <NavBar />
      <Hero />
-      </div>
+     
       </>
     );
 }
